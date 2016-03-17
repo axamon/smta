7b348e698c0dd712b97a1511d84bccbfab43ae83
axamon <alberto.bregliano@gmail.com>
2016-03-17 19:10:51 +0100

    Update robot2.py

--- robot2.py
+++ robot2.py
@@ -8,55 +8,67 @@ import redis
 rlocal = redis.StrictRedis(host='vocvideo.ddns.net')
 
 while True:
-	idvideoteca=str(rlocal.brpoplpush('filmdatestare','filmdatestare',0))
-	print idvideoteca
-	print '''
-	Sintassi: <username> <password> <id videoteca>	
-	
-	Copyright Alberto Bregliano
-	'''
-	time.sleep(3)
-	profile = webdriver.FirefoxProfile()
-	profile.set_preference('plugin.state.npctrl', 2)
-	browser = webdriver.Firefox(profile)
-	browser.maximize_window()
-	browser.get('http://www.timvision.it')
-	accettacookie = browser.find_element_by_xpath('//*[@id="cookiebox"]/p[2]/a')
-	type(accettacookie)
 	try:
-    		accettacookie.click()
+		idvideoteca=str(rlocal.brpoplpush('filmdatestare','filmdatestare',0))
+		print idvideoteca
+		print '''
+		Sintassi: <username> <password> <id videoteca>	
+		
+		Copyright Alberto Bregliano
+		'''
+		time.sleep(3)
+		profile = webdriver.FirefoxProfile()
+		profile.set_preference('plugin.state.npctrl', 2)
+		browser = webdriver.Firefox(profile)
+		browser.maximize_window()
+		browser.get('http://www.timvision.it')
+		accettacookie = browser.find_element_by_link_text('OK')
+		type(accettacookie)
+		try:
+				accettacookie.click()
+		except:
+				pass
+		#accettacookie.click()
+		browser.maximize_window()
+		time.sleep(5)
+		accedi = browser.find_element_by_link_text('Accedi')
+		type(accedi)
+		try:
+			accedi.click()
+		except:
+			pass
+		time.sleep(1)
+		username = browser.find_element_by_id('loginUsernameId')
+		try:
+			username.send_keys(rlocal.get('username'))
+		except:
+			pass
+		time.sleep(2)
+		pwd =browser.find_element_by_id('loginPwdId')
+		try:
+			pwd.send_keys(rlocal.get('password'))
+		except:
+			pass
+		time.sleep(2)
+		accedi2 = browser.find_element_by_id('loginBtn')
+		type(accedi2)
+		try:
+			accedi2.click()
+		except:
+			pass
+		time.sleep(2)
+		browser.get('http://www.timvision.it/detail/'+idvideoteca)
+		time.sleep(2)
+		#guarda = browser.find_element_by_xpath('//*[@id="spct"]/div[3]/div/div[5]/a[3]')
+		#guarda = browser.find_elements_by_class_name('button asset')
+		guarda = browser.find_element_by_link_text('Guarda')
+		type(guarda)
+		try:
+				guarda.click()
+		except:
+			pass
+		#guarda.click()
+		time.sleep(30)
+		browser.quit()
 	except:
-       		self.browser.execute_script(
-			 '$("{sel}").click()'.format(sel=el_selector)
-        		)
-	#accettacookie.click()
-	browser.maximize_window()
-	accedi = browser.find_element_by_link_text('Accedi')
-	type(accedi)
-	accedi.click()
-	time.sleep(1)
-	username = browser.find_element_by_id('loginUsernameId')
-	username.send_keys(rlocal.get('username'))
-	time.sleep(2)
-	pwd =browser.find_element_by_id('loginPwdId')
-	pwd.send_keys(rlocal.get('password'))
-	time.sleep(2)
-	accedi2 = browser.find_element_by_id('loginBtn')
-	type(accedi2)
-	accedi2.click()
-	time.sleep(2)
-	browser.get('http://www.timvision.it/detail/'+idvideoteca)
-	time.sleep(2)
-	#guarda = browser.find_element_by_xpath('//*[@id="spct"]/div[3]/div/div[5]/a[3]')
-	#guarda = browser.find_elements_by_class_name('button asset')
-	guarda = browser.find_element_by_link_text('Guarda')
-	type(guarda)
-	try:
-    		guarda.click()
-	except:
-    		self.browser.execute_script(
-			 	'$("{sel}").click()'.format(sel=el_selector)
-        			)
-	#guarda.click()
-	time.sleep(300)
-	browser.quit()
+		pass
