b9a99b0c321735c8cfee63bb929091b0a475d93a
root <root@smta.homenet.telecomitalia.it>
2016-03-18 21:33:34 +0100

    buon test

--- test.py
+++ test.py
@@ -1,36 +1,63 @@
 from scapy.all import TCP, IP, sniff
 from scapy_http import http
+import redis
+
+rlocal = redis.StrictRedis(host='vocvideo.ddns.net')
+
+def update_stats(context, value, type='test'):
+        destination = 'stats:%s:%s'%(context, type)
+        #rlocal.lpush(destination+':tempi', value)
+        rlocal.hsetnx(destination, 'min', value)
+        rlocal.hsetnx(destination, 'max', value)
+        if value < float(rlocal.hget(destination, 'min')):
+                rlocal.hset(destination, 'min', value)
+        if value > float(rlocal.hget(destination, 'max')):
+                rlocal.hset(destination, 'max', value)
+        count = int(rlocal.hincrby(destination, 'count',1))
+        sum = float(rlocal.hincrbyfloat(destination, 'sum', value))
+        sumq = float(rlocal.hincrbyfloat(destination, 'sumq', value*value))
+        avg = sum/count
+        stddev = float(((sumq / count) - (avg ** 2)) ** .5)
+        rlocal.hset(destination,'avg',avg)
+        rlocal.hset(destination,'stddev',stddev)
 
 def process_tcp_packet(packet):
     '''
     Sniffa il traffico e recupera la richiesta del manifest
     '''
-    #if not packet.haslayer(http.HTTPRequest):
-#	return
+    #if not packet.haslayer(http):
+    #	return
     if packet.haslayer(http.HTTPRequest):
     	http_layer = packet.getlayer(http.HTTPRequest)
     	ip_layer = packet.getlayer(IP)
 	tcp_layer = packet.getlayer(TCP)
-    	request = '{1[Host]}{1[Path]}'.format(ip_layer.fields, http_layer.fields)
-	tcpwindow = '{[window]}'.format(tcp_layer.fields)
+	global request
+    	request = 'http://{1[Host]}{1[Path]}'.format(ip_layer.fields, http_layer.fields)
+	global idvideoteca
+	idvideoteca = str(request.split("/")[8])
+	global requestwindow
+	requestwindow = '{[window]}'.format(tcp_layer.fields)
+	global requestack
 	requestack = '{[ack]}'.format(tcp_layer.fields)
-    	print requestack, packet.time, tcpwindow, request
-	#print packet.show()
+	global requesttime
+	requesttime = float(packet.time)
+    	#print requestack, packet.time, tcpwindow, request
+#	print packet.show()
 
     if packet.haslayer(http.HTTPResponse):
     	http_layer = packet.getlayer(http.HTTPResponse)
     	ip_layer = packet.getlayer(IP)
 	tcp_layer = packet.getlayer(TCP)
-    	response = '{1[Status-Line]},{2[window]}'.format(ip_layer.fields, http_layer.fields, tcp_layer.fields)
+    	response = '{1[Status-Line]}'.format(ip_layer.fields, http_layer.fields).split()[1]
 	tcpwindow = '{[window]}'.format(tcp_layer.fields)
 	responseseq = '{[seq]}'.format(tcp_layer.fields)
-    	print responseseq, packet.time, tcpwindow, response
+	if requestack == responseseq:
+		tts = float(packet.time)-requesttime
+    		print idvideoteca, request, tts, requestwindow, response
+		try:
+			update_stats(idvideoteca,tts)
+		except:
+			pass
 	#print packet.show()
-
-    #if packet.haslayer(TCP):
-#	tcp_layer = packet.getlayer(TCP)
-#    	ip_layer = packet.getlayer(IP)
-##	tcpwindow = '{[window]}'.format(tcp_layer.fields)
-##	print tcpwindow
 
 sniff(filter='tcp and port 80', prn=process_tcp_packet, store=0)
